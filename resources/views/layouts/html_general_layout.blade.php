42902089d3acb5a890ed3e85e45610754f25af4d
Paul Guillard <paul@benuvillageesch.lu>
2022-01-14 14:52:51 -0800

    Switching to Laravel. Landing page reproduced with no difference. Font Barlow imported via Google Fonts in head. Laravel Breeze and Livewire installed.

---
+++ resources/views/layouts/html_general_layout.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        @yield('og-metadata')
+
+        <title>@yield('title')</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;600;700&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@300;400;500;600&display=swap" rel="stylesheet"> 
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/style_landing_page.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+
+        <!-- Font awesome icons -->
+        <script src="https://kit.fontawesome.com/983b1bd0fa.js" crossorigin="anonymous"></script>
+
+        <!-- Livewire Styles -->
+        @livewireStyles
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </head>
+
+    <body class="antialiased overflow-x-hidden">
+        <div class="min-h-screen">
+            @yield('header')
+
+            <!-- Page Content -->
+            <main>
+                @yield('main-content')
+            </main>
+        </div>
+        
+        @include('footer.footer')
+
+        <!-- Scripts -->
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        @livewireScripts
+        @yield('scripts')
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,105 @@
+@extends('layouts.html_general_layout')
+
+@section('title')
+    Bienvenue sur BENU COUTURE
+@endsection
+
+@section('header')
+    <header>
+        <div class="header_bckgnd">
+            <div class="header_bckgnd__font">
+                <div class="header_bckgnd__moving header_bckgnd__moving--1">
+                    <img src="{{ asset('images/benu_landing_clouds_illustration.svg') }}" style="height: 100%;" />
+                </div>
+                <div class="header_bckgnd__moving header_bckgnd__moving--2">
+                    <img src="{{ asset('images/benu_landing_clouds_illustration.svg') }}" style="height: 100%;" />
+                </div>
+            </div>
+            <div class="header_bckgnd__bird" id="header-bird-pic">
+                <img src="{{ asset('images/benu_landing_illustration.svg') }}" style="height: 280px;" />
+            </div>
+        </div>
+        
+        <div class="header_logo">
+            <img src="{{ asset('images/logo_benu_couture_blanc.svg') }}" style="height: 225px;" />
+        </div>
+        <div class="header_socialmedia">
+            <p>
+                <a href="https://www.facebook.com/benuvillageesch/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+            </p>
+            <p>
+                <a href="https://www.instagram.com/benu_village/" target="_blank"><i class="fab fa-instagram"></i></a>
+            </p>
+            <p>
+                <a href="https://benu.lu/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+            </p>
+        </div>
+    </header>
+@endsection
+
+@section('main-content')
+    <section class="pattern_bg">
+        <div class="central_col">
+            <div class="central_textbox">
+                <div class="central_textbox__teaser">
+                    <p>
+                        Soon
+                    </p>
+                </div>
+                <h1>BENU COUTURE</h1>
+                <p class="central_textbox__sub">
+                    Lancement du site&nbsp;: 2ème trimestre 2022
+                </p>
+                <p class="central_textbox__desc">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+            <div class="contact_form_container">
+                <div>
+                    <h2 class="text-center">
+                        Restons en contact
+                    </h2>
+                    <!-- <div>
+                        <form method="POST" class="contact_form">
+                            <input type="email" name="newsletter_email" placeholder="Je m'inscris à la newsletter">
+                            <input type="submit" name="newsletter_btn" value="Je m'inscris">
+                        </form>
+                    </div> -->
+                    <div class="contact-link text-center">
+                        <a href="https://benu.lu/fr/contactez-nous/" target="_blank">Je m'inscris à la newsletter</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="universe">
+        <div class="text-center">
+            <h2>
+                L'univers BENU
+            </h2>
+            <div class="universe__links">
+                <div>
+                    <a href="https://www.benureuse.lu/fr/" class="btn-large btn-yellow" target="_blank">BENU REUSE</a>
+                </div>
+                <div>
+                    <a href="https://benu.lu/" class="btn-large btn-green" target="_blank">BENU VILLAGE</a>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('scripts')
+    <script type="text/javascript">
+        $(function() {
+            let newTop = '130px';
+            $('#header-bird-pic').css('top', newTop);
+            setInterval(function() {
+                newTop = 60 + 150*Math.random();
+                newTop += 'px';
+                $('#header-bird-pic').css('top', newTop);
+            }, 2500);
+        });
+    </script>
+@endsection
