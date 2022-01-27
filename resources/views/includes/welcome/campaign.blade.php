6b4f90d1519b86572b1593510067cc73927c08cf
Paul Guillard <paul@benuvillageesch.lu>
2022-01-27 12:03:46 +0100

    Update of pictures on the welcome page.

--- resources/views/includes/welcome/campaign.blade.php
+++ resources/views/includes/welcome/campaign.blade.php
@@ -5,7 +5,7 @@
 			<p class="welcome-campaign__left__title-2">{{ __('welcome.campaign-title-2') }}</p>
 			<div>
 				<div>
-					<img src="{{ asset('images/pictures/campaign-1.png') }}" class="m-auto">
+					<img src="{{ asset('images/pictures/welcome-campaign-1.jpg') }}" class="m-auto">
 				</div>
 				<div class="flex justify-center welcome-campaign__left__links">
 					<a href="#" class="block btn-couture">{{ __('welcome.campaign-link-1') }}</a>
@@ -14,7 +14,7 @@
 			</div>
 		</div>
 		<div class="welcome-campaign__right">
-			<img src="{{ asset('images/pictures/campaign-2.png') }}" class="m-auto">
+			<img src="{{ asset('images/pictures/welcome-campaign-2.jpg') }}" class="m-auto">
 		</div>
 	</div>
 </section>
