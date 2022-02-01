61f6c7491b61f9d5f9cc94af5ef502503191ea85
Paul Guillard <paul@benuvillageesch.lu>
2022-02-01 17:32:29 +0100

    Added translation keys for the whole welcome page, including header and footer.

---
+++ resources/lang/de/components.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Components Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are used on specific components, such as models and articles display
+|
+*/
+
+return [
+    "models-header" => "components.models-header",
+    "models-model" => "components.models-model",
+];
