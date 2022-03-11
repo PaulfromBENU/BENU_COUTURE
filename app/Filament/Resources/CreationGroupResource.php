28ee15393f421224a7c1f58fcc5e19f60fee7585
Paul Guillard <paul@benuvillageesch.lu>
2022-03-11 14:32:17 +0100

    Minor CSS update for correct fit in all languages. Types added to Filament back-office.

---
+++ app/Filament/Resources/CreationGroupResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CreationGroupResource\Pages;
+use App\Filament\Resources\CreationGroupResource\RelationManagers;
+use App\Models\CreationGroup;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+
+class CreationGroupResource extends Resource
+{
+    protected static ?string $label = 'type pour les créations';
+    protected static ?string $pluralLabel = 'types pour les créations';
+
+    protected static ?string $model = CreationGroup::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name_fr')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('name_lu')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('name_de')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('name_en')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('translation_key')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('filter_key')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name_fr'),
+                Tables\Columns\TextColumn::make('name_lu'),
+                Tables\Columns\TextColumn::make('name_de'),
+                Tables\Columns\TextColumn::make('name_en'),
+                Tables\Columns\TextColumn::make('translation_key'),
+                Tables\Columns\TextColumn::make('filter_key'),
+            ])
+            ->filters([
+                //
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCreationGroups::route('/'),
+            'create' => Pages\CreateCreationGroup::route('/create'),
+            'edit' => Pages\EditCreationGroup::route('/{record}/edit'),
+        ];
+    }
+}
