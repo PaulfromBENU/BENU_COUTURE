2e4b45dcdfd8fadb19b9a526ca4cd71af8268632
Paul Guillard <paul@benuvillageesch.lu>
2022-03-21 10:59:46 +0100

    Added URL query string update when filters are applied on all creations page.

--- app/Http/Livewire/Filters/FilteredModels.php
+++ app/Http/Livewire/Filters/FilteredModels.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Filters;
 
 use Livewire\Component;
+use Illuminate\Http\Request;
 
 use App\Models\Creation;
 
@@ -20,28 +21,48 @@ class FilteredModels extends Component
     public $initial_load;
     public $applied_filters;
 
+    // Required to persist filters in the URL dynamically
+    public $family;
+    public $types;
+    public $categories;
+    public $colors;
+    public $prices;
+    public $shops;
+
     public $paginate_pages_count;
     public $paginate_page;
 
+    protected $queryString = [
+        'paginate_page'  => ['except' => 1, 'as' => 'page'],
+        'family'  => ['except' => ''],
+        'types'  => ['except' => ''],
+        'categories'  => ['except' => ''],
+        'colors'  => ['except' => ''],
+        'prices'  => ['except' => ''],
+        'shops'  => ['except' => ''],
+    ];
+
     protected $listeners = ['filtersUpdated' => 'resetPagination', 'sortUpdated' => 'changeSorting'];
 
-    public function mount()
+    public function mount(Request $request)
     {
         $this->sort_direction = 'asc';
+        if (isset($request->page)) {
+            $this->paginate_page = intval($request->page);
+        } else {
+            $this->paginate_page = 1;
+        }
+        $this->adaptQueryFilters($this->initial_filters);
         $this->applyFilters($this->initial_filters);
         $this->initial_load = 1;
-        $this->paginate_page = 1;
     }
-
-    // public function loadInitialModels() //On init, with wire:init
-    // {
-    //     $this->applyFilters($this->initial_filters);
-    //     $this->initial_load = 1;
-    // }
 
     public function applyFilters($applied_filters)
     {
         $this->applied_filters = $applied_filters;
+
+        $this->adaptQueryFilters($this->applied_filters);
+
         //Initialize filtered models
         $this->filtered_models = collect([]);
         
@@ -49,6 +70,10 @@ class FilteredModels extends Component
         $this->filtered_models = $this->getFilteredCreations($applied_filters);
 
         $this->paginate_pages_count = intval(floor($this->filtered_models->count() / 12) + 1);
+        // Check paginate_page value in case request has wrong data
+        if ($this->paginate_page < 1 || $this->paginate_page > $this->paginate_pages_count) {
+            $this->paginate_page = 1;
+        }
         
         $this->sortAndDivide();
     }
@@ -113,6 +138,66 @@ class FilteredModels extends Component
 
         $this->applyFilters($this->applied_filters); //Necessary to avoid models conversion to array
         $this->emit('pageChanged');
+    }
+
+    public function adaptQueryFilters($filters)
+    {
+        $this->family = '';
+        $this->categories = '';
+        $this->types = '';
+        $this->colors = '';
+        $this->sizes = '';
+        $this->prices = '';
+        $this->shops = '';
+        // foreach ($filters['family'] as $family => $value) {
+        //     if ($value == 1) {
+        //         $this->family .= $family;
+        //     }
+        // }
+        foreach ($filters['categories'] as $category => $value) {
+            if ($value == 1) {
+                $this->categories .= $category.'*';
+            }
+        }
+        if (substr($this->categories, -1) == "*") {
+            $this->categories = substr($this->categories, 0, -1);
+        }
+
+        foreach ($filters['types'] as $type => $value) {
+            if ($value == 1) {
+                $this->types .= $type.'*';
+            }
+        }
+        if (substr($this->types, -1) == "*") {
+            $this->types = substr($this->types, 0, -1);
+        }
+
+        foreach ($filters['colors'] as $color => $value) {
+            if ($value == 1) {
+                $this->colors .= $color.'*';
+            }
+        }
+        if (substr($this->colors, -1) == "*") {
+            $this->colors = substr($this->colors, 0, -1);
+        }
+
+        foreach ($filters['prices'] as $price => $value) {
+            if ($value == 1) {
+                $this->prices .= $price.'*';
+            }
+        }
+        if (substr($this->prices, -1) == "*") {
+            $this->prices = substr($this->prices, 0, -1);
+        }
+
+        foreach ($filters['shops'] as $shop => $value) {
+            if ($value == 1) {
+                $this->shops .= $shop.'*';
+            }
+        }
+        if (substr($this->shops, -1) == "*") {
+            $this->shops = substr($this->shops, 0, -1);
+        }
     }
 
     public function render()
