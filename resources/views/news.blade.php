c9f3db519ebad02391787710abfbad763e8863f1
Paul Guillard <paul@benuvillageesch.lu>
2022-04-15 18:11:19 +0200

    Updated all news page design. Static data, database saving of news still to be implemented, and specific news page to be updated.

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -21,14 +21,32 @@
 @endsection
 
 @section('main-content')
-	<div class="text-center all-news w-1/2 m-auto">
+	<div class="text-center all-news w-2/3 m-auto">
 		<h4 class="all-news__subtitle">{{ __('news.all-subtitle') }}</h4>
 		<h2 class="all-news__title">{{ __('news.all-title') }}</h2>
 
-		<div>
+		<div class="flex justify-start flex-wrap">
 			@for($i = 1; $i <= 5; $i++)
 			<a href="{{ route('news-'.app()->getLocale(), ['slug' => 'premier-article-benu-couture']) }}" class="all-news__link">
-				Titre de l'actualité {{ $i }}
+				<div class="all-news__link__img-container">
+					<img src="{{ asset('images/pictures/news/image_trees.png') }}" alt="Photo news" title="Photo news">
+				</div>
+				<div class="all-news__link__tags flex justify-start">
+					@for($j = 1; $j <= 2; $j++)
+					<div class="all-news__link__tags__tag">
+						Tag line {{ $j }}
+					</div>
+					@endfor
+				</div>
+				<h3 class="all-news__link__title">
+					Titre de l'actualité {{ $i }}
+				</h3>
+				<p class="all-news__link__date">
+					15 avril 2021
+				</p>
+				<p class="all-news__link__summary">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				</p>
 			</a>
 			@endfor
 		</div>
