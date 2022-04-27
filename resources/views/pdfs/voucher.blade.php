3d76f0417f7d4777fc8838409c05094df91982bd
Paul Guillard <paul@benuvillageesch.lu>
2022-04-27 09:36:49 +0200

    Title change in voucher pdf.

--- resources/views/pdfs/voucher.blade.php
+++ resources/views/pdfs/voucher.blade.php
@@ -4,7 +4,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Votre facture BENU</title>
+	<title>Bon d'achat BENU</title>
 
 	<style type="text/css">
 		@font-face {
