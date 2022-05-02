238598e4e07c0060f30f0501213ec6ddb3602ddc
Paul Guillard <paul@benuvillageesch.lu>
2022-05-02 15:19:15 +0200

    Added extra explanation text for extra accessories.

--- resources/views/includes/model/model_extras.blade.php
+++ resources/views/includes/model/model_extras.blade.php
@@ -1,7 +1,10 @@
-<section class="model-articles" id="model-extra-accessories">
+<section class="model-articles benu-container" id="model-extra-accessories">
 	@if($extra_accessories->count() > 0)
-	<h2>{{ __('models.extra-articles-title') }}</h2>
-	<div class="model-articles__list flex flex-wrap justify-start benu-container">
+	<h2 style="margin-bottom: 40px;">{{ __('models.extra-articles-title') }}</h2>
+	<p class="text-center w-1/2 m-auto pb-10 font-medium">
+		{{ __('models.extra-articles-explanation') }}
+	</p>
+	<div class="model-articles__list flex flex-wrap justify-start pt-10">
 	    @foreach($extra_accessories as $article)
 	        @livewire('components.article-overview', ['article' => $article], key('extra-'.$article->id))
 	    @endforeach
