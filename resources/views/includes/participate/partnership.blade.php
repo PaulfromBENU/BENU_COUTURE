cf29c8c3faf666fefdafc27852f84e1fd196cd09
Paul Guillard <paul@benuvillageesch.lu>
2022-06-14 11:15:43 +0200

    Minor display update on Participate page.

--- resources/views/includes/participate/partnership.blade.php
+++ resources/views/includes/participate/partnership.blade.php
@@ -13,9 +13,12 @@
 	<p class="participate-partnership__content">
 		{{ __('participate.partnership-content-3') }}
 	</p>
-	<p class="participate-partnership__highlight">
-		{{ __('participate.partnership-highlight-2') }} <a href="{{ route('client-service-'.app()->getLocale(), ['page' => __('slugs.services-contact')]) }}" class="primary-color hover:text-gray-800 transition">{{ __('participate.partnership-highlight-2-link') }}</a>
+	<p class="text-center m-10">
+		<a href="{{ route('client-service-'.app()->getLocale(), ['page' => __('slugs.services-contact')]) }}" class="btn-couture-plain btn-couture-plain--fit btn-couture-plain--dark-hover">{{ __('participate.partnership-highlight-2-link') }}</a>
 	</p>
+	<!-- <p class="participate-partnership__highlight">
+		{{ __('participate.partnership-highlight-2') }} <a href="{{ route('client-service-'.app()->getLocale(), ['page' => __('slugs.services-contact')]) }}" class="primary-color hover:text-gray-800 transition">{{ __('participate.partnership-highlight-2-link') }}</a>
+	</p> -->
 	<p class="participate-partnership__subtitle">
 		{{ __('participate.partnership-subtitle-4') }}
 	</p>
