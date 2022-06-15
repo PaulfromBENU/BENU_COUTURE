1aa653be6bd9ebf5434059b6f38bcccc82434431
Paul Guillard <paul@benuvillageesch.lu>
2022-06-15 11:33:43 +0200

    Added wikipedia link to vouchers.

--- resources/views/includes/vouchers/voucher_desc.blade.php
+++ resources/views/includes/vouchers/voucher_desc.blade.php
@@ -13,6 +13,23 @@
 		<p class="model-pres__desc__txt-vouchers">
 			{{ __('vouchers.desc-txt-3') }}
 		</p>
+
+		<div>
+			<p class="model-pres__desc__txt" style="margin-bottom: 10px;">
+				{!! __('vouchers.link-explanation') !!}
+			</p>
+			@php
+			$localized_links = [
+				'en' => 'https://en.wikipedia.org/wiki/Lesser_bilby',
+				'fr' => 'https://fr.wikipedia.org/wiki/Macrotis_leucura',
+				'de' => 'https://de.wikipedia.org/wiki/Kleiner_Kaninchennasenbeutler',
+				'lu' => 'https://de.wikipedia.org/wiki/Kleiner_Kaninchennasenbeutler',
+			];
+			@endphp
+			<p class="model-pres__desc__link">
+				<a href="{{ $localized_links[app()->getLocale()] }}" target="_blank" class="btn-slider-left">{{ __('vouchers.model-origins') }} BILBY</a>
+			</p>
+		</div>
 		
 		<div class="flex model-pres__desc__seemore">
 			<a onclick='document.getElementById("voucher-options").scrollIntoView({ behavior: "smooth", block: "start" });' class="flex">
