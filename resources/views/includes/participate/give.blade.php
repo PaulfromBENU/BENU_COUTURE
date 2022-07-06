49df5d74ba3a4ae5adf9d85d159c833ae3b9ac13
Paul Guillard <paul@benuvillageesch.lu>
2022-07-06 12:08:04 +0200

    Updated 'Participate' page.

--- resources/views/includes/participate/give.blade.php
+++ resources/views/includes/participate/give.blade.php
@@ -3,27 +3,36 @@
 
 	<div class="participate-give__content">
 		<p class="participate-give__content__txt">
-			{{ __('participate.give-content-1.1') }}
+			{{ __('participate.give-content-1-1') }} <a href="#" class="primary-color hover:text-gray-800 transition">{{ __('participate.give-content-1-1-link') }}</a>
 		</p>
 		<p class="participate-give__content__txt">
-			{{ __('participate.give-content-1.2') }}
+			{{ __('participate.give-content-1-2') }} <a href="#" class="primary-color hover:text-gray-800 transition">{{ __('participate.give-content-1-2-link') }}</a>
 		</p>
-		<p class="participate-give__content__txt">
-			{{ __('participate.give-content-1.3') }}
-		</p>
-		<div class="text-center mt-5 pt-5">
+<!-- 		<div class="text-center mt-5 pt-5">
 			<a href="#" class="btn-couture-plain btn-couture-plain--fit btn-couture-plain--dark-hover">{{ __('participate.give-link-1') }}</a>
-		</div>
+		</div> -->
 	</div>
 
 	<div class="participate-give__content">
-		<p class="participate-give__content__txt">
-			{{ __('participate.give-content-2.1') }}
+		<p class="participate-partnership__subtitle">
+			{{ __('participate.give-subtitle-2') }}
 		</p>
 		<p class="participate-give__content__txt">
-			{{ __('participate.give-content-2.2') }}
+			{{ __('participate.give-content-2-1') }}
 		</p>
-		<div class="text-center mt-5 pt-5">
+		<p class="participate-give__content__txt">
+			{{ __('participate.give-content-2-2') }}
+		</p>
+		<p class="participate-give__content__txt">
+			{{ __('participate.give-content-2-3') }}
+		</p>
+		<p class="participate-give__content__txt">
+			{{ __('participate.give-content-2-4') }}
+		</p>
+		<p class="participate-give__content__txt">
+			{{ __('participate.give-content-2-5') }}
+		</p>
+		<div class="text-left mt-5 pt-5">
 			<a href="#" class="btn-couture-plain btn-couture-plain--fit btn-couture-plain--dark-hover">{{ __('participate.give-link-2') }}</a>
 		</div>
 	</div>
