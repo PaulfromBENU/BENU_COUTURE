2930a405b0864fa92e228e543fae990bf9a87bd6
Paul Guillard <paul@benuvillageesch.lu>
2022-07-07 12:10:44 +0200

    Update of Participate->give page with contacts design.

--- resources/views/includes/participate/give.blade.php
+++ resources/views/includes/participate/give.blade.php
@@ -40,12 +40,112 @@
 	<h2 class="participate-give__title pt-10 mt-10">{{ __('participate.give-title-2') }}</h2>
 	<div class="participate-give__content">
 		<p class="participate-give__content__txt">
-			{{ __('participate.give-content-3-1') }}
+			{!! __('participate.give-content-3-1') !!}
 		</p>
 	</div>
-	<div>
-		<p>
-			Design points de collecte a définir - Kamoo
-		</p>
+
+
+	<div class="participate-give__collect-box">
+		<h3>
+			BENU Shop Esch-sur-Alzette
+		</h3>
+		<div class="participate-give__collect-box__details">
+			<p class="mb-3">
+				<strong>{!! __('services.shops-address') !!} : </strong>  51 rue d'Audun, 4018 Esch-sur-Alzette, Luxembourg
+			</p>
+			<div class="flex">
+				<p class="w-3/5">
+					<strong>{!! __('services.shops-phone') !!} :</strong> +352 27 91 19 49
+				</p>
+				<p class="w-2/5">
+					<strong>{!! __('services.shops-website') !!} : </strong> <a href="https://www.benu.lu" target="_blank">www.benu.lu</a>
+				</p>
+			</div>
+		</div>
+		<div class="flex justify-around">
+			<p class="mt-2 mb-2">
+				<a href="{{ route('client-service-'.app()->getLocale(), ['page' => __('slugs.services-shops')]) }}" class="btn-couture-plain btn-couture-plain--fit btn-couture-plain--dark-hover">{!! __('participate.give-opening-time') !!}</a>
+			</p>
+		</div>
+	</div>
+
+	<div class="participate-give__collect-box">
+		<h3>
+			Gérard Kieffer
+		</h3>
+		<div class="participate-give__collect-box__details">
+			<p class="mb-3">
+				<strong>{!! __('services.shops-address') !!} : </strong>  53, rue de Remich, L- 5330 Moutfort, Luxembourg
+			</p>
+			<div class="flex">
+				<p class="w-3/5">
+					<strong>{!! __('services.shops-phone') !!} :</strong> +352 691 35 87 13
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="participate-give__collect-box">
+		<h3>
+			Jean-Marie Kieffer
+		</h3>
+		<div class="participate-give__collect-box__details">
+			<p class="mb-3">
+				<strong>{!! __('services.shops-address') !!} : </strong>  4, Ancien Chemin d'Osweiler, L-6469 Echternach, Luxembourg
+			</p>
+			<div class="flex">
+				<p class="w-3/5">
+					<strong>{!! __('services.shops-phone') !!} :</strong> +352 72 82 42
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="participate-give__collect-box">
+		<h3>
+			Executive Textil Atelier - Sylvie Hamus
+		</h3>
+		<div class="participate-give__collect-box__details">
+			<p class="mb-3">
+				<strong>{!! __('services.shops-address') !!} : </strong>  49, rue Victor Muller-Fromes, L-9261 Diekirch, Luxembourg
+			</p>
+			<div class="flex">
+				<p class="w-3/5">
+					<strong>{!! __('services.shops-phone') !!} :</strong> +352 26 80 32 50
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="participate-give__collect-box">
+		<h3>
+			Naturpark Our - Christian Kayser
+		</h3>
+		<div class="participate-give__collect-box__details">
+			<p class="mb-3">
+				<strong>{!! __('services.shops-address') !!} : </strong>  12, Parc, L-9836 Hosingen, Luxembourg
+			</p>
+			<div class="flex">
+				<p class="w-3/5">
+					<strong>{!! __('services.shops-phone') !!} :</strong> +352 90 81 88-633
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="participate-give__collect-box">
+		<h3>
+			BENU VILLAGE ESCH asbl - Georges Kieffer
+		</h3>
+		<div class="participate-give__collect-box__details">
+			<p class="mb-3">
+				<strong>{!! __('services.shops-address') !!} : </strong>  50, rue des Celtes, L-1318 Luxembourg, Luxembourg
+			</p>
+			<div class="flex">
+				<p class="w-3/5">
+					<strong>{!! __('services.shops-phone') !!} :</strong> +352 6 21 18 81 05
+				</p>
+			</div>
+		</div>
 	</div>
 </section>
