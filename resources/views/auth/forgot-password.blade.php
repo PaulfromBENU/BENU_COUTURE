0161807b70995eb2eabf2dfddde151006032b4e2
Paul Guillard <paul@benuvillageesch.lu>
2022-07-07 09:58:34 +0200

    Updated Forgotten password and New password request pages (translations and breadcrumbs).

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.base_layout')
 
 @section('title')
-    BENU COUTURE - Mot de passe oublié
+    {{ __('auth.password-forgotten-seo-title') }}
 @endsection
 
 @section('description')
-    Utilisez votre adresse e-mail pour définir un nouveau mot de passe
+    {{ __('auth.password-forgotten-seo-description') }}
 @endsection
 
 @section('robots-behaviour')
@@ -19,7 +19,7 @@ noindex, nofollow
             <div class="pl-5 pr-5">
                 >
             </div>
-            <a href="{{ route('login-'.app()->getLocale()) }}" class="primary-color"><strong>{{ __('breadcrumbs.forgotten-pwd') }}</strong></a>
+            <a href="{{ route('password.request-'.app()->getLocale()) }}" class="primary-color"><strong>{{ __('breadcrumbs.forgotten-pwd') }}</strong></a>
         </div>
     </div>
 @endsection
@@ -28,35 +28,36 @@ noindex, nofollow
     <section class="benu-container login">
         <h3 class="login__subtitle">BENU COUTURE</h3>
         @if(session('status') == null)
-        <h1 class="login__title">J'ai oublié mon <br/>mot de passe</h1>
+        <h1 class="login__title">{{ __('auth.password-forgotten-title-1') }} <br/>{{ __('auth.password-forgotten-title-2') }}</h1>
 
-        <form method="POST" action="{{ route('password.email', ['locale' => app()->getLocale()]) }}" class="w-1/4 m-auto mb-10">
+        <form method="POST" action="{{ route('password.email', ['locale' => app()->getLocale()]) }}" class="login__forgotten-form m-auto mb-10">
             @csrf
             <div class="input-group reactive-label-input">
-                <label for="email">Adresse e-mail *</label>
+                <label for="email">{{ __('auth.password-forgotten-email') }} *</label>
                 <input type="email" id="email" name="email" class="input-underline w-full" required>
             </div>
 
             <p class="login__info">
-                <em>* Champs obligatoires</em>
+                <em>* {{ __('auth.password-forgotten-mandatory-fields') }}</em>
             </p>
             
             <div class="m-auto login__validate">
-                <input type="submit" name="login_submit" class="btn-couture-plain" value="Mettre à jour mon mot de passe">
+                <input type="submit" name="login_submit" class="btn-couture-plain" value="{{ __('auth.password-forgotten-update-password-btn') }}">
                 <div class="login__validate__question">
-                    Je n’ai pas encore de compte BENU COUTURE.
+                    {{ __('auth.password-forgotten-no-account-yet') }}
                 </div>
-                <a href="{{ route('register-'.app()->getLocale()) }}" class="btn-slider-left mt-3">Je crée mon compte ici</a>
+                <a href="{{ route('register-'.app()->getLocale()) }}" class="btn-slider-left mt-3">{{ __('auth.password-forgotten-create-account') }}</a>
             </div>
             @if($errors->any())
                 {!! implode('', $errors->all('<div>:message</div>')) !!}
             @endif
         </form>
         @else
-        <h1 class="login__title">Tu as reçu un e-mail pour <br/>réinitialiser ton mot de passe</h1>
+        <h1 class="login__title">{{ __('auth.password-forgotten-email-info-1') }} <br/>{{ __('auth.password-forgotten-email-info-2') }}</h1>
 
         <p class="font-medium w-1/2 m-auto">
-            Dans cet e-mail, tu trouveras un lien sécurisé, valable une heure, qui te permettra de choisir un nouveau mot de passe. Si tu ne voies pas l'e-mail dans ta boite de réception, vérifie qu'il n'a pas atterri par erreur dans ton courrier indésirable&nbsp;!
+            <!-- Dans cet e-mail, tu trouveras un lien sécurisé, valable une heure, qui te permettra de choisir un nouveau mot de passe. Si tu ne voies pas l'e-mail dans ta boite de réception, vérifie qu'il n'a pas atterri par erreur dans ton courrier indésirable&nbsp;! -->
+            {{ __('auth.password-forgotten-email-info-3') }}
         </p>
         @endif
     </section>
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.base_layout')
 
 @section('title')
-    BENU COUTURE - Mot de passe oublié
+    {{ __('auth.password-reset-seo-title') }}
 @endsection
 
 @section('description')
-    Utilisez votre adresse e-mail pour définir un nouveau mot de passe
+    {{ __('auth.password-reset-seo-description') }}
 @endsection
 
 @section('robots-behaviour')
@@ -19,7 +19,7 @@ noindex, nofollow
             <div class="pl-5 pr-5">
                 >
             </div>
-            <a href="{{ route('login-'.app()->getLocale()) }}" class="primary-color"><strong>{{ __('breadcrumbs.forgotten-pwd') }}</strong></a>
+            <a href="{{ route('password.reset-'.app()->getLocale()) }}" class="primary-color"><strong>{{ __('breadcrumbs.forgotten-pwd') }}</strong></a>
         </div>
     </div>
 @endsection
@@ -27,31 +27,31 @@ noindex, nofollow
 @section('main-content')
     <section class="benu-container login">
         <h3 class="login__subtitle">BENU COUTURE</h3>
-        <h1 class="login__title">Je choisis un nouveau <br/>mot de passe</h1>
+        <h1 class="login__title">{{ __('auth.password-reset-title-1') }} <br/>{{ __('auth.password-reset-title-2') }}</h1>
 
         <form method="POST" action="{{ route('password.update', ['locale' => app()->getLocale()]) }}" class="w-1/4 m-auto mb-10">
             @csrf
             <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
             <div class="input-group reactive-label-input">
-                <label for="email">Adresse e-mail *</label>
+                <label for="email">{{ __('auth.password-reset-email') }} *</label>
                 <input type="email" id="email" name="email" class="input-underline w-full" required>
             </div>
             <div class="input-group reactive-label-input">
-                <label for="password">Nouveau mot de passe *</label>
+                <label for="password">{{ __('auth.password-reset-new-password') }} *</label>
                 <input type="password" id="password" name="password" class="input-underline w-full" required>
             </div>
             <div class="input-group reactive-label-input">
-                <label for="password_confirmation">Confirmation nouveau mot de passe *</label>
+                <label for="password_confirmation">{{ __('auth.password-reset-new-password-conf') }} *</label>
                 <input type="password" id="password_confirmation" name="password_confirmation" class="input-underline w-full" required>
             </div>
 
             <p class="login__info">
-                <em>* Champs obligatoires</em>
+                <em>* {{ __('auth.password-reset-mandatory-fields') }}</em>
             </p>
             
             <div class="m-auto login__validate">
-                <input type="submit" name="login_submit" class="btn-couture-plain" value="Mettre à jour mon mot de passe">
+                <input type="submit" name="login_submit" class="btn-couture-plain" value="{{ __('auth.password-reset-update-password-info') }}">
             </div>
             @if($errors->any())
                 {!! implode('', $errors->all('<div>:message</div>')) !!}
