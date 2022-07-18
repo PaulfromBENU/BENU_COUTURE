cb5185205fc4bd6b8e794338074c4784f2a5bdeb
Paul Guillard <paul@benuvillageesch.lu>
2022-07-18 16:36:13 +0200

    Creation of 'About' Page. Minor design update on welcome page.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -21,42 +21,262 @@
 @endsection
 
 @section('main-content')
-<section class="w-3/5 m-auto full-story mb-10 pb-10">
-	<h2 class="full-story__subtitle">À propos de BENU</h2>
-	<h1 class="full-story__title">Faciliter le don de meubles et d'objets d'occasion au Luxembourg </h1>
+<section class="w-3/5 m-auto full-story mb-10 pb-10 benu-container">
+	<h2 class="full-story__subtitle">{{ __('about.subtitle-top') }}</h2>
+	<h1 class="full-story__title">{{ __('about.title-top') }}</h1>
 
-	<p class="full-story__txt">
-		Chez BENU REUSE, tu peux facilement rechercher et commander des articles d'occasion depuis chez toi. Tu peux proposer des choses dont tu n’as plus besoin et les transmettre aux clients intéressés. Que ce soit en tant que particulier, salarié*e dans un organisme public ou une société commerciale au Luxembourg : T’es invité*e à rejoindre BENU REUSE.
-	</p>
-	<p class="full-story__highlight">
-		Réutilisons et réduisons les déchets ensemble !
-	</p>
-	<p class="full-story__txt">
-		L'utilisation multiple des matériaux usagés et des objets du quotidien permet d'économiser matières premières, énergie, émissions et déchets. Moins de nouvelles productions, un recyclage moins gourmand en ressources et moins d'espace d'enfouissement sont nécessaires.<br/> Les articles d'occasion encore en bon état, propres, complets et fonctionnels sont vendus via BENU REUSE. Diverses choses ne sont pas acceptées, par exemple les appareils nécessitant une réparation ou les matériaux contaminés par des polluants. Tu peux en savoir plus sur les critères d'un placement via BENU REUSE ici. 
-	</p>
+	<h3 class="full-story__section-title">
+		{{ __('about.section-title-1') }}
+	</h3>
 
-	<p class="full-story__txt">
-		Contrairement aux plateformes commerciales conventionnelles, tous les objets et matériaux sont transmis gratuitement à BENU REUSE. Notre équipe BENU REUSE récupère les articles proposés et les livre directement aux clients. Ceux-ci paient des frais de livraison avec lesquels nous couvrons une partie de nos frais de logistique et de transport. Étudiant*e, les organismes à but non lucratif bénéficient d'une réduction sur les frais de livraison. Nos réponses aux questions les plus fréquemment posées expliquent comment fonctionnent la recherche, la commande et l'offre chez BENU REUSE.
-	</p>
+	<div class="full-story__general flex justify-between">
+		<div class="full-story__general__menu-container">
+			<div class="full-story__general__menu-container__menu">
+				<ul>
+					<li>
+						<button class="btn-slider-left text-lg font-bold">Titre 1</button>
+					</li>
+					<li>
+						<button class="btn-slider-left text-lg font-bold">Titre 2</button>
+					</li>
+					<li>
+						<button class="btn-slider-left text-lg font-bold">Titre 3</button>
+					</li>
+					<li>
+						<button class="btn-slider-left text-lg font-bold">Titre 4</button>
+					</li>
+				</ul>
+			</div>
+		</div>
 
-	<p class="full-story__txt">
-		Avec BENU REUSE, nous voulons sensibiliser au thème de la réduction des déchets, de la réutilisation et de la consommation durable. Nous t’ invitons à parcourir, à en savoir plus, à faire preuve de créativité et à redécouvrir ton appréciation de ce que tu ass procuré. BENU REUSE n'est qu'une des nombreuses structures luxembourgeoises actives dans le domaine du recyclage. Sous REUSE Service, tu peux savoir où les articles usagés sont acceptés, transmis ou réparés au Luxembourg.
-	</p>
+		<div class="full-story__general__content">
+			<div class="full-story__general__content__slider">	
+				<div class="full-story__general__content__slider__cards flex justify-start">
+					@for($i = 1; $i <= 7; $i++)
+					<div class="full-story__general__content__slider__cards__card" @if($i == 1) id="slider-card-1" @endif>
+						<img src="" />
+						<div class="full-story__general__content__slider__cards__card__footer flex justify-start">
+							<div class="flex flex-col justify-center">
+								<p class="full-story__general__content__slider__cards__card__footer--number">
+									{{ $i }}
+								</p>
+							</div>
+							<div class="full-story__general__content__slider__cards__card__footer--txt flex flex-col justify-center">
+								<p>
+									{{ __('about.card-txt-'.$i) }}
+								</p>
+							</div>
+						</div>
+					</div>
+					@endfor
+				</div>
+			</div>
+			<div class="full-story__general__content__slider__bar flex justify-between">
+				<div class="flex justify-start w-1/5">
+					<div class="full-story__general__content__slider__bar__btn full-story__general__content__slider__bar__btn--left">
+						@svg('chevron-down', 'full-story__general__content__slider__bar__svg')
+					</div>
+					<div class="full-story__general__content__slider__bar__btn full-story__general__content__slider__bar__btn--right">
+						@svg('chevron-down', 'full-story__general__content__slider__bar__svg')
+					</div>
+				</div>
+				<div class="full-story__general__content__slider__bar__scroll w-4/5">
+					<div class="absolute full-story__general__content__slider__bar__scroll--red"></div>
+				</div>
+			</div>
 
-	<p class="full-story__txt">
-		BENU REUSE est un projet pilote en phase de test et est continuellement développé, par exemple en évaluant les informations actuelles sur la gestion du recyclage et le recyclage. T’es cordialement invités à participer, que ce soit dans le cadre du REUSE Service ou grâce à tes connaissances spécialisées. Si t’as des informations ou des connaissances approfondies sur des études, par exemple sur les critères de durabilité, alors rejoigne-nous et deviens un membre actif de la communauté BENU REUSE. Contacte-nous ici et nous discuterons ensemble de la manière dont tes expériences chez BENU REUSE peuvent être intégrées.
-	</p>
-	
-	<p class="full-story__txt text-center">
-		Tu veux en savoir plus sur BENU REUSE ?
-	</p>
 
-	<p class="text-center mb-10">
-		<a href="{{ route('full-story-'.app()->getLocale()) }}" class="btn-couture m-auto">Alors lis toute l'histoire !</a>
-	</p>
+			<h3 class="full-story__section-title">
+				{{ __('about.section-title-2') }}
+			</h3>
+			<div class="full-story__general__content__info-section">
+				<div class="full-story__general__content__info-section__picture">
+					<img src="{{ asset('images/pictures/footer-img-1.png') }}">
+				</div>
+				<div class="full-story__general__content__info-section__txt">
+					<h4>{{ __('about.section-2-subtitle-1') }}</h4>
+
+					<p class="full-story__general__content__info-section__txt__paragraph">
+						{{ __('about.section-2-txt-1') }}
+					</p>
+
+					<div class="flex justify-between flex-wrap mb-10">
+						<div class="w-2/5">
+							<img src="{{ asset('images/pictures/vouchers_img.png') }}">
+						</div>
+						<div class="w-3/5 pl-5">
+							<h5>
+								{{ __('about.section-2-lowtitle-1') }}
+							</h5>
+							<p class="full-story__general__content__info-section__txt__paragraph">
+								{{ __('about.section-2-txt-2-1') }}
+							</p>
+							<p class="full-story__general__content__info-section__txt__paragraph">
+								{{ __('about.section-2-txt-2-2') }}
+							</p>
+							<p class="full-story__general__content__info-section__txt__paragraph">
+								{{ __('about.section-2-txt-2-3') }}
+							</p>
+						</div>
+					</div>
+
+					<div class="flex justify-between flex-wrap pt-10">
+						<div class="w-3/5 pr-5">
+							<h4>{{ __('about.section-2-subtitle-2') }}</h4>
+							<p class="full-story__general__content__info-section__txt__paragraph">
+								{{ __('about.section-2-txt-3-1') }} <a href="#" class="primary-color hover:text-gray-800 transition">{{ __('about.section-2-txt-3-link') }}</a>
+							</p>
+							<p class="full-story__general__content__info-section__txt__paragraph">
+								{{ __('about.section-2-txt-3-2') }}
+							</p>
+						</div>
+						<div class="w-2/5">
+							<img src="{{ asset('images/pictures/vouchers_img.png') }}">
+						</div>
+					</div>
+				</div>
+
+				<div class="full-story__general__content__info-section__transition">
+					<img src="{{ asset('images/pictures/footer-img-1.png') }}">
+				</div>
+
+				<div class="full-story__general__content__info-section__txt">
+					<h4>{{ __('about.section-2-subtitle-3') }}</h4>
+
+					<p class="full-story__general__content__info-section__txt__paragraph">
+						{{ __('about.section-2-txt-4') }}
+					</p>
+
+					<div class="flex justify-between flex-wrap mb-10">
+						<div class="w-2/5">
+							<img src="{{ asset('images/pictures/vouchers_img.png') }}">
+						</div>
+						<div class="w-3/5 pl-5">
+							<h4>{{ __('about.section-2-subtitle-4') }}</h4>
+							<p class="full-story__general__content__info-section__txt__paragraph">
+								{{ __('about.section-2-txt-5-1') }}
+							</p>
+							<p class="full-story__general__content__info-section__txt__paragraph">
+								{{ __('about.section-2-txt-5-2') }}
+							</p>
+						</div>
+					</div>
+
+					<h4>{{ __('about.section-2-subtitle-4') }}</h4>
+
+					<p class="full-story__general__content__info-section__txt__paragraph">
+						{{ __('about.section-2-txt-6') }}
+					</p>
+
+					<p class="full-story__general__content__info-section__txt__paragraph">
+						{{ __('about.section-2-txt-7') }}
+					</p>
+
+					<p class="full-story__general__content__info-section__txt__highlight">
+						{{ __('about.section-2-txt-8') }}
+					</p>
+
+					<p class="full-story__general__content__info-section__txt__paragraph">
+						{{ __('about.section-2-txt-9') }}
+					</p>
+
+					<p class="text-center mb-10">
+						<a href="{{ route('client-service-'.app()->getLocale(), ['page' => __('slugs.services-contact')]) }}" target="_blank" class="btn-couture-plain btn-couture-plain--fit btn-couture-plain--dark-hover">{{ __('about.section-2-btn-contact') }}</a>
+					</p>
+					
+				</div>
+			</div>
+
+
+			<h3 class="full-story__section-title">
+				{{ __('about.section-title-3') }}
+			</h3>
+
+
+			<div class="full-story__general__content__info-section">
+				<div class="full-story__general__content__info-section__picture">
+					<img src="{{ asset('images/pictures/footer-img-1.png') }}">
+				</div>
+				<div class="full-story__general__content__info-section__txt">
+					<p class="full-story__general__content__info-section__txt__paragraph">
+						{{ __('about.section-3-txt-1') }}
+					</p>
+
+					<p class="w-1/2 m-auto text-center mb-10">
+						<img src="{{ asset('images/pictures/footer-img-1.png') }}">
+					</p>
+
+					<div class="flex justify-between flex-wrap mb-10">
+						<div class="w-2/5">
+							<img src="{{ asset('images/pictures/vouchers_img.png') }}">
+						</div>
+						<div class="w-3/5 pl-5">
+							<p class="full-story__general__content__info-section__txt__paragraph">
+								{{ __('about.section-3-txt-2-1') }}
+							</p>
+							<p class="full-story__general__content__info-section__txt__paragraph">
+								{{ __('about.section-3-txt-2-2') }}
+							</p>
+						</div>
+					</div>
+
+					<div class="flex justify-between flex-wrap pt-10">
+						<div class="w-3/5 pr-5">
+							<p class="full-story__general__content__info-section__txt__paragraph">
+								{{ __('about.section-2-txt-3-1') }} <a href="#" class="primary-color hover:text-gray-800 transition">{{ __('about.section-2-txt-3-link') }}</a>
+							</p>
+							<p class="full-story__general__content__info-section__txt__paragraph">
+								{{ __('about.section-2-txt-3-2') }}
+							</p>
+							<p class="full-story__general__content__info-section__txt__paragraph">
+								{{ __('about.section-2-txt-3-2') }}
+							</p>
+						</div>
+						<div class="w-2/5">
+							<img src="{{ asset('images/pictures/vouchers_img.png') }}">
+						</div>
+					</div>
+				</div>
+				<p class="text-center mt-10 mb-10">
+					<a href="{{ route('client-service-'.app()->getLocale(), ['page' => __('slugs.services-contact')]) }}" target="_blank" class="btn-couture-plain btn-couture-plain--fit btn-couture-plain--dark-hover">{{ __('about.section-2-btn-contact') }}</a>
+				</p>
+			</div>
+		</div>
+	</div>
 
 </section>
 @endsection
 
 @section('scripts')
+<script type="text/javascript">
+	$(function() {
+		$('.full-story__general__content__slider__bar__btn--right').on('click', function() {
+			let leftPos = $('.full-story__general__content__slider').scrollLeft();
+  			$('.full-story__general__content__slider').animate({scrollLeft: leftPos + 100}, 'fast');
+
+  			let leftPosScroll = parseInt($('.full-story__general__content__slider__bar__scroll--red').css('left'));
+  			if (leftPosScroll < ($('.full-story__general__content__slider__bar__scroll').width() * 0.75)) {
+  				$('.full-story__general__content__slider__bar__scroll--red').animate({left: leftPosScroll + 80}, 'fast');
+  			} else {
+  				$('.full-story__general__content__slider__bar__scroll--red').css('left', $('.full-story__general__content__slider__bar__scroll').width() * 0.75);
+  			}
+	 	});
+	 	$('.full-story__general__content__slider__bar__btn--left').on('click', function() {
+			let leftPos = $('.full-story__general__content__slider').scrollLeft();
+  			$('.full-story__general__content__slider').animate({scrollLeft: leftPos - 100}, 'fast');
+
+  			let leftPosScroll = $('.full-story__general__content__slider__bar__scroll--red').position().left;
+  			// console.log(leftPosScroll, $('.full-story__general__content__slider__bar__scroll').width());
+  			if ($('.full-story__general__content__slider__bar__scroll--red').position().left >= 80) {
+  				$('.full-story__general__content__slider__bar__scroll--red').animate({left: leftPosScroll - 80}, 'fast');
+  			} else {
+  				$('.full-story__general__content__slider__bar__scroll--red').css('left', 0);
+  			}
+	 	});
+
+	 	$('.full-story__general__content__slider').on('scroll', function() {
+	 		$('.full-story__general__content__slider__bar__scroll--red').css('left', $('.full-story__general__content__slider').scrollLeft() * 0.8);
+	 	})
+	});
+</script>
 @endsection
