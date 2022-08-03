d476dc6190cb3c2b392bd116f1ce3120aeb8efd2
Paul Guillard <paul@benuvillageesch.lu>
2022-08-03 14:02:41 +0200

    Error message on wrong credentials translated.

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -51,7 +51,7 @@ class LoginRequest extends FormRequest
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'email' => __('auth.failed'),
+                'email' => trans("auth.failed", [], session('locale')),
             ]);
         }
 
