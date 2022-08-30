02e380a9789c2a4b1e9bf8426ec6e362b35bb41d
Paul Guillard <paul@benuvillageesch.lu>
2022-08-30 15:40:57 +0200

    Added sponsor logo at the bottom of the pages.

--- resources/views/footer/all_infos.blade.php
+++ resources/views/footer/all_infos.blade.php
@@ -10,4 +10,20 @@
 		<a href="{{ route('header.participate-'.app()->getLocale()) }}" class="btn-couture btn-couture--transparent">{{ __('footer.all-chart') }}</a>
 		<a href="{{ route('client-service-'.app()->getLocale(), ['page' => 'faq']) }}" class="btn-couture btn-couture--transparent">{{ __('footer.all-faq') }}</a>
 	</div>
+
+	<div class="footer-all__sponsor">
+		<div class="benu-container flex justify-between flex-col lg:flex-row" style="height: 100%;">
+			<div class="flex flex-col lg:flex-row justify-start" style="height: 100%;">
+				<div class="lg:mr-10 footer-all__sponsor__logo">
+					<img src="{{ asset('images/pictures/logo-oeuvre-gdc.jpg') }}">
+				</div>
+				<div class="text-center lg:text-left flex flex-col justify-center">
+					<p>{{ __('welcome.sponsor-thanks-1') }}</p>
+				</div>
+			</div>
+			<div class="text-center lg:text-left flex lg:flex-col justify-center" style="min-width: 200px;">
+				<a href="https://www.oeuvre.lu/" target="_blank" class="btn-couture btn-couture-plain--dark-hover" style="margin-bottom: 0;">{{ __('welcome.sponsor-link-1') }}</a>
+			</div>
+		</div>
+	</div>
 </section>
