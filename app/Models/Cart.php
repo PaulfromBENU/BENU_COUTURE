d74bb501fa7b742eb9af9a36a513658710c36b27
Paul Guillard <paul@benuvillageesch.lu>
2022-09-30 19:17:05 +0200

    Idem but accessible with App helper.

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Facades\App;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,7 +17,7 @@ class Cart extends Model
 
     public function couture_variations()
     {
-        if(env('APP_ENV') == 'stage') {
+        if(App::environment('stage')) {
             return $this->belongsToMany(Article::class, 'benu_common_stage.couture_article_cart')->withPivot('is_gift', 'with_wrapping', 'with_card', 'card_type', 'with_message', 'message', 'with_extra_article', 'articles_number', 'value');
         }
         return $this->belongsToMany(Article::class, 'benu_common.couture_article_cart')->withPivot('is_gift', 'with_wrapping', 'with_card', 'card_type', 'with_message', 'message', 'with_extra_article', 'articles_number', 'value');
--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\CanResetPassword;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\App;
 
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Models\Contracts\HasName;
@@ -106,7 +107,7 @@ class User extends Authenticatable implements HasName, FilamentUser
 
     public function wishlistArticles()
     {
-        if(env('APP_ENV') == 'stage') {
+        if(App::environment('stage')) {
             return $this->belongsToMany('App\Models\Article', 'benu_common_stage.couture_article_user');
         }
         return $this->belongsToMany('App\Models\Article', 'benu_common.couture_article_user');
