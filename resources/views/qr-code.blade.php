93af4cd1aa5ebe082298ac1b439db224fcdaf9fd
Paul Guillard <paul@benuvillageesch.lu>
2022-10-26 11:28:40 +0200

    Updated QR Code generation.

--- resources/views/qr-code.blade.php
+++ resources/views/qr-code.blade.php
@@ -5,7 +5,9 @@
     @for($i = 1; $i <= $number; $i ++)
     <div class="visible-print text-center qr-code__code">
         {!! QrCode::size(100)->generate(route('model-fr', ['name' => $creation_name])); !!}
-        <p class="text-center primary-color">{{ strtoupper($creation_name) }}</p>
+        <p class="text-center primary-color qr-code__code__name">{{ strtoupper($creation_name) }}</p>
+        <p class="text-center primary-color qr-code__code__price">{{ $price }}&euro;</p>
+        <!-- format('png')->merge('/public/images/favicon/apple-icon-60x60.png')-> -->
     </div>
     @endfor
 </div>
